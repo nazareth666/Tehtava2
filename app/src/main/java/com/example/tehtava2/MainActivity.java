7566f088a0201f3a571b1cdc71ddc75fafd6b380
nazareth666 <46488067+nazareth666@users.noreply.github.com>
2020-01-20 14:46:16 +0200

    valmis

--- app/src/main/java/com/example/tehtava2/MainActivity.java
+++ app/src/main/java/com/example/tehtava2/MainActivity.java
@@ -11,19 +11,53 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Toolbar toolbar = findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+        
 
+        findViewById(R.id.button_center).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Button btn2 = findViewById(R.id.button_iso);
+                Button btn3 = findViewById(R.id.button_pieni);
+
+                if(btn2.isEnabled() && btn3.isEnabled()){
+                    btn2.setEnabled(false);
+                    btn3.setEnabled(false);
+                }
+                else {
+                    btn2.setEnabled(true);
+                    btn3.setEnabled(true);
+                }
+            }
+        });
+        findViewById(R.id.button_pieni).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView imgView = findViewById(R.id.imageView);
+                imgView.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        findViewById(R.id.button_iso).setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                ImageView imgView = findViewById(R.id.imageView);
+                imgView.setVisibility(View.VISIBLE);
+                return false;
+            }
+        });
     }
 
-    @Override
+
+
+        @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
